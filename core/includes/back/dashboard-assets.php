f122e1f12aefc6fe425e2355c13bc9aaf9db2104
vitaliikaplia <vitalii.kaplia@gmail.com>
2023-08-08 15:32:58 +0300

    allow custom select2 only for custom configuration page

--- core/includes/back/dashboard-assets.php
+++ core/includes/back/dashboard-assets.php
@@ -6,7 +6,10 @@ if(!defined('ABSPATH')){exit;}
 function custom_dashboard_assets(){
     wp_register_style( 'custom-dashboard-css', TEMPLATE_DIRECTORY_URL . 'assets/css/dashboard.min.css', false, ASSETS_VERSION);
     wp_enqueue_style( 'custom-dashboard-css' );
-    wp_register_script( 'custom-dashboard-js', TEMPLATE_DIRECTORY_URL . 'assets/js/dashboard.min.js', '', ASSETS_VERSION, true);
-    wp_enqueue_script( 'custom-dashboard-js' );
+    global $pagenow;
+    if($pagenow == "index.php"){
+        wp_register_script( 'custom-dashboard-js', TEMPLATE_DIRECTORY_URL . 'assets/js/dashboard.min.js', '', ASSETS_VERSION, true);
+        wp_enqueue_script( 'custom-dashboard-js' );
+    }
 }
 add_action( 'admin_enqueue_scripts', 'custom_dashboard_assets' );
