bb6ef995deb2529f52092f5f7fd8c4520b19c4cb
vitaliikaplia <vitalii.kaplia@gmail.com>
2024-03-05 21:58:34 +0200

    fixed redirect rules

--- core/includes/back/system-redirect-rules.php
+++ core/includes/back/system-redirect-rules.php
@@ -2,18 +2,19 @@
 
 if(!defined('ABSPATH')){exit;}
 
-$general_fields = cache_general_fields();
+add_action('init', function () {
+    $general_fields = cache_general_fields();
+    if (!empty($general_fields['redirect_rules']) && is_array($general_fields['redirect_rules'])) {
+        $REQUEST_URI = rtrim($_SERVER['REQUEST_URI'], '/') . '/';
 
-if (!empty($general_fields['redirect_rules']) && is_array($general_fields['redirect_rules'])) {
-    $REQUEST_URI = rtrim($_SERVER['REQUEST_URI'], '/') . '/';
+        foreach ($general_fields['redirect_rules'] as $rule) {
+            $old = str_replace(["https://", "http://", BLOGINFO_JUST_DOMAIN], "", rtrim($rule['old_url'], '/') . '/');
+            $new = $rule['new_url'];
 
-    foreach ($general_fields['redirect_rules'] as $rule) {
-        $old = str_replace(["https://", "http://", BLOGINFO_JUST_DOMAIN], "", rtrim($rule['old_url'], '/') . '/');
-        $new = $rule['new_url'];
-
-        if ($REQUEST_URI === $old) {
-            wp_redirect($new, 301);
-            exit;
+            if ($REQUEST_URI === $old) {
+                wp_redirect($new, 301);
+                exit;
+            }
         }
     }
-}
+});
